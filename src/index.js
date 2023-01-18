2088258235e155937d029750745e4a72c881015b
Martin Karugaba <martinkarugaba21@gmail.com>
2023-01-18 15:14:48 +0300

    set up and style todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-console.log('Hello webpack');
-console.log('I care');
-console.log('see ya');
+import './index.css';
+import Icon from './img/refresh.png';
+
+const todoList = document.querySelector('.todoList__wrapper');
+
+const todoArray = [
+  {
+    index: 0,
+    description: 'Buy Groceries',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'Go to the gym',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Have lunch',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Take an afternoon nap',
+    completed: false,
+  },
+];
+
+todoArray.map((item) => {
+  const { id, description, completed } = item;
+
+  todoList.innerHTML += `
+    <div class="todo">
+      <input type="checkbox" name="" id="" />
+      <p class="description">${description}</p>
+      <div class="dots">
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </div>
+    </div>
+  `;
+});
