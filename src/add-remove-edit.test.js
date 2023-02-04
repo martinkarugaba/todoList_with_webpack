bceca59bdc560ec9f91ac22af5c876e1c2b3b926
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-02-04 08:46:47 +0000

    Complete test for check if a to do has been removed from the DOM

--- src/add-remove-edit.test.js
+++ src/add-remove-edit.test.js
@@ -24,3 +24,15 @@ describe('addTodo', () => {
     expect(mockLocalStorage).toHaveLength(1);
   });
 });
+
+describe('remove todo', () => {
+  test('check if a todo has been removed from the DOM', () => {
+    document.body.innerHTML = `
+      <div class="todoList__wrapper">
+      </div>`;
+    const todoList = document.querySelector('.todoList__wrapper');
+    removeTodo(todoList, 1);
+    const todos = todoList.querySelectorAll('.todo');
+    expect(todos).toHaveLength(0);
+  });
+});
